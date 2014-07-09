6cb530209847a53b6a222e423c174440b6e03bbd
Imti <imtiazmaj@gmail.com>
2014-07-08 17:54:43 -0700

    Multi-line text on click

--- src/main.js
+++ src/main.js
@@ -3,76 +3,46 @@ define(function(require, exports, module) {
     var Surface    = require('famous/core/Surface');
     var Modifier   = require('famous/core/Modifier');
     var Transform  = require('famous/core/Transform');
-    var Quaternion = require('famous/math/Quaternion');
-    var Draggable  = require("famous/modifiers/Draggable");
-    // var MouseSync = require('famous/inputs/MouseSync');
-    var ScrollSync = require('famous/inputs/ScrollSync');
+    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var RenderNode = require('famous/core/RenderNode');
 
     var mainContext = Engine.createContext();
 
-    var quaternion = new Quaternion(1, 0, 0, 0);
-    var smallQuaternion = new Quaternion(100, 0, 1, 0);
-    var sync = new ScrollSync();
-    console.log(smallQuaternion);
-
-    // var draggable = new Draggable({
-    //     xRange: [-1000, 1000],
-    //     yRange: [-1000, 1000]
-    // });
-
-    // Modifier != StateModifier
-    // Modifiers can move, StateModifiers can not
-    var rotationModifier = new Modifier({
-        origin: [0.5, 0.5]
-
+    var modifier = new Modifier({
+        origin: [0.5, 0.5],
+        align: [0.5, 0.5],
+        size: [window.innerWidth, window.innerHeight]
     });
 
-    var position = [0, 0];
-    var quaternionToggle
-
-    rotationModifier.setTransform(function() {
-        return quaternion.getTransform();
-    });
-
-    Engine.on('prerender', function() {
-        // console.log(smallQuaternion.w);
-        smallQuaternion.w = smallQuaternion.w < 50 ? smallQuaternion.w+.05 : 100;
-        quaternion = quaternion.multiply(smallQuaternion);
-    });
-
-
-    var square = new Surface({
-        size: [300, 300],
-        content: 'Hello Famo.us',
+    var slide = new Surface({
+        size: [undefined, undefined],
+        content: "Make a Presentation Using Famo.us",
         classes: ['double-sided'],
         properties: {
-            fontSize: '30px',
-            lineHeight: '300px',
-            textAlign: 'center',
+            fontSize: window.innerWidth/15 + 'px',
+            lineHeight: window.innerWidth/10 + 'px',
+            textAlign: 'center'
         },
-        transform: Transform.translate(position[0], position[1], 300/2)
-
     });
-    square.pipe(sync);
-
-    sync.on('update', function(data) {
-        // console.log(data);
-        position[0] += data.delta[0];
-        position[1] += data.delta[1];
-        smallQuaternion.w = position[0]/(position[0] + position[1]); 
-        console.log(smallQuaternion.w);
-        quaternion = quaternion.multiply(smallQuaternion);
+    var logo = new ImageSurface({
+        size: [200, 200],
+        content: 'http://code.famo.us/assets/famous_logo.svg',
+        classes: ['double-sided']
     });
 
-    // square.pipe(draggable);
+    var text = [
+        "Each slide is a surface",
+        "Set new content on <br/> Engine click event",
+        "And it's not just text!",
+        "",
+    ];
 
-    // square.on('mouseover', function() {
-    //     console.log("test");
-    //     var x = (Math.random() * 2) - 1;
-    //     var y = (Math.random() * 2) - 1;
-    //     var z = (Math.random() * 2) - 1;
-    //     smallQuaternion = new Quaternion(100, x, y, z);
-    // });
+    var next = 0;
 
-    mainContext.add(rotationModifier).add(sync).add(square);
+    Engine.on('click', function() {
+        slide.setContent(slide.getContent() + '<br/>' + text[next]);
+        next++;
+    })
+
+    mainContext.add(modifier).add(slide);
 });
