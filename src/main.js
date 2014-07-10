fd31f51cc2e87083c0d1a5f31fcc599c37e99f05
Imti <imtiazmaj@gmail.com>
2014-07-09 22:31:41 -0700

    Finishing touches, keeping this presentation simple

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 define(function(require, exports, module) {
+    // Requires
     var Engine     = require('famous/core/Engine');
     var Surface    = require('famous/core/Surface');
     var Modifier   = require('famous/core/Modifier');
@@ -6,29 +7,35 @@ define(function(require, exports, module) {
     var ImageSurface = require('famous/surfaces/ImageSurface');
     var RenderController = require('famous/views/RenderController');
 
+    // Context, renderController
     var mainContext = Engine.createContext();
     var renderController = new RenderController();
     var slides = [];
     var counter = 0;
 
+    // text content for each surface
     var text = [
         "<br/><br/> Make a Presentation Using Famo.us",
         "<br/><br/> So I met Dave Fetterman today...",
         "<br/><br/> He thought it'd be a cool idea to <br/> create a powerpoint-esque presentation...",
         "<br/><br/> ...entirely in Famo.us",
+        "<br/><br/> So here it is",
         "<br/><br/> Each slide is a surface",
         "<br/><br/> And click events switch to a new surface",
         "<br/><br/> Animations can also be added",
-        "<br/><br/> Like this",
-        "<br/><br/> And this",
-        "<br/><br/> And also this"
+        "<br/><br/> But we'll keep this one simple",
+        "<br/><br/> Until next time...",
+        "<br/><br/> End"
     ];
 
+    // apply modifier to entire renderController
     var slideModifier = new Modifier({
         origin: [0.5, 0.5],
         align: [0.5, 0.5]
     });
 
+    // For each text content, make a new 
+    //surface and set it to the text
     for(var i = 0; i < text.length; i++) {
         slides.push(new Surface({
             size: [undefined, undefined],
@@ -42,13 +49,16 @@ define(function(require, exports, module) {
         }));
     }
 
+    // show first slide 
     renderController.show(slides[0]);
 
+    // simulate powerpoint by showing the 
+    // next slide on a click event on Engine
     Engine.on('click', function() {
-        var next = (counter++ +1) % slides.length;
-        this.show(slides[next]);
+        var next = (counter++ +1);
+        if(next < slides.length) this.show(slides[next]);
     }.bind(renderController));
+    // bind renderController to preserve 'this'
 
-   
     mainContext.add(slideModifier).add(renderController);
 });
